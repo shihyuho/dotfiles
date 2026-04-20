8854b954c8fe9b4f85d29e12d985a71b062453de
Shihyu Ho <methodho@gmail.com>
2026-04-20 10:46:15 +0800

    feat: add cdg function to jump to git repo root

--- zsh/aliases/navigation.zsh
+++ zsh/aliases/navigation.zsh
@@ -17,3 +17,13 @@ alias dl="cd $HOME/Downloads"
 alias doc="cd $HOME/Documents"
 alias cfg="cd $HOME/.config"
 alias tmp="cd $HOME/tmp"
+
+# Jump to the git repository root
+cdg() {
+  local root
+  root=$(git rev-parse --show-toplevel 2>/dev/null) || {
+    print -u2 "cdg: not in a git repository"
+    return 1
+  }
+  cd "$root"
+}
